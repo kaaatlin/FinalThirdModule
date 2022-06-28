dbd6bec74f10a971b6ab16c9e5439fe8a826d390
Катерина С <sidorcheva2001@mail.ru>
2022-06-28 14:07:31 +0300

    Добавьте файлы проекта.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        enum Days
+        {
+            понедельник = 1,
+            вторник,
+            среда,
+            четверг,
+            пятница,
+            суббота,
+            воскресенье
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Ваш возраст: ");
+            byte age = checked ((byte)int.Parse(Console.ReadLine()));
+
+            if (age < 18)
+            {
+                Console.WriteLine("Вас зовут {0} и Вы несовершеннолетний", name);
+            }
+            else
+            {
+                Console.WriteLine("Вас зовут {0} и Вы совершеннолетний", name);
+            }
+
+            Console.Write("Ваш рост: ");
+            double high = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ваш рост составляет " + high);
+
+            Console.Write("Какой ваш любимый день недели? ");
+            Days FavDay = (Days) int.Parse(Console.ReadLine());
+            Console.WriteLine("Ваш любимый день недели " + FavDay);
+
+            Console.ReadKey();
+        }
+    }
+}
